ee80cffb6a245955313b6fd7356600017a27f239
Deepak973 <deepak.rj.dr@gmail.com>
2024-09-03 17:46:45 +0530

    aaded layerzero package

--- script/StarGateTransferV2.sol
+++ script/StarGateTransferV2.sol
@@ -10,17 +10,20 @@ contract StargateTransferScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployerAddress = vm.addr(deployerPrivateKey);
-
+        address stargatePoolUSDC =0xc026395860Db2d07ee33e05fE50ed7bD583189C7;
+        uint256 destinationEndpointId =40231; //arbitrum
+        uint256 amount =1000000; //1 USDC
+        address sourceChainPoolToken =0x488327236B65C61A6c083e8d811a4E0D3d1D4268; //USDC
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy the StargateTransfer contract
         StargateIntegration integration = new StargateIntegration();
 
         // as Alice
-        ERC20(sourceChainPoolToken).approve(stargate, amount);
+        IERC20(sourceChainPoolToken).approve(stargatePoolUSDC, amount);
 
         (uint256 valueToSend, SendParam memory sendParam, MessagingFee memory messagingFee) =
-        integration.prepareTakeTaxi(stargate, destinationEndpointId, amount, deployerAddress);
+        integration.prepareTakeTaxi(stargatePoolUSDC, destinationEndpointId, amount, deployerAddress);
 
         IStargate(stargate).sendToken{ value: valueToSend }(sendParam, messagingFee, deployerAddress);
 
